74ff57efdfebdddcf24d98b61855c380537f06b3
mocamoca <noi_kaas@yahoo.co.jp>
2018-03-07 14:53:36 +0900

    検索機能を追加

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,9 @@ class IndexView(generic.ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.order_by('-created_at')
+        # return Post.objects.order_by('-created_at') 記事一覧を最新順で表示を行う
+        queryset = Post.objects.order_by('-created_at')
+        keyword = self.request.GET.get('keyword')
+        if keyword:
+            queryset = queryset.filter(title=keyword) #titleで絞りたい場合
+        return queryset
